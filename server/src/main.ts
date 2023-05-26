5096544bedc4edb87912493946bc188cb53a9374
DongHyun <leedonghyun022@gmail.com>
2023-05-26 18:10:47 +0900

    feat: swagger (ui) 추가

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,13 @@
 import fastify from 'fastify';
 import { router } from './route/router';
 import { authPlugin } from './plugin/auth';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUi from '@fastify/swagger-ui';
 
 const app = fastify({ logger: true });
 
+app.register(fastifySwagger);
+app.register(fastifySwaggerUi);
 app.register(router);
 app.register(authPlugin);
 
