ac2ea212295e26abcc85cac64943948070a56f1a
DongHyun <leedonghyun022@gmail.com>
2023-05-30 17:10:18 +0900

    feat: ux 개선

--- client/src/page/Search.tsx
+++ client/src/page/Search.tsx
@@ -5,10 +5,11 @@ import { yogiPokeApi } from "../service/api";
 import { DomainBottomNavigation } from "./MyPage";
 import { useNotification } from "../component/Notification";
 import { AxiosError } from "axios";
-import { useDeferredValue, useState, useTransition } from "react";
+import { useState } from "react";
 import useSWR from "swr";
 import { UserListItem } from "../component/UserListItem";
 import { useDebouncedValue } from "../hook/useDebouncedValue";
+import { validator } from "../service/validator";
 
 type User = {
   email: string;
@@ -23,12 +24,20 @@ export const Search = () => {
   assertAuth();
 
   const [email, setEmail] = useState("");
-  const deferredEmail = useDebouncedValue(email, 200);
+  const deferredEmail = useDebouncedValue(email, 300);
   const [selectedEmail, setSelectedEmail] = useState<null | string>(null);
 
   const { data, isLoading } = useSWR<User[]>(
-    ["/user", { email: deferredEmail, limit: 5 }],
-    { keepPreviousData: true }
+    deferredEmail.length === 0 || validator.email(deferredEmail) === null
+      ? ["/user", { email: deferredEmail, limit: 5 }]
+      : [],
+    {
+      keepPreviousData: true,
+      onSuccess: (users) => {
+        users.every((user) => user.email !== selectedEmail) &&
+          setSelectedEmail(null);
+      },
+    }
   );
 
   const { trigger, isMutating } = useSWRMutation(
@@ -39,6 +48,9 @@ export const Search = () => {
         err.response?.status === 404
           ? push({ content: "존재하지 않는 유저입니다!" })
           : push({ content: "다시 시도해주세요." }),
+      onSuccess: () => {
+        push({ content: "콕 찌르기 성공!" });
+      },
     }
   );
 
@@ -60,23 +72,21 @@ export const Search = () => {
           />
         </div>
         <div className="mt-5 flex flex-col">
-          {["korean_bill_gates", "asdffg", "wgefdf"]
-            .map((user) => user + deferredEmail)
-            .map((user, i) => (
-              <UserListItem
-                key={user}
-                listIndex={i}
-                onClick={() => setSelectedEmail(user)}
-                selected={selectedEmail === user}
-                userEmail={user}
-                userName={user}
-              />
-            ))}
+          {data?.map((user, i) => (
+            <UserListItem
+              key={user.email}
+              listIndex={i}
+              onClick={() => setSelectedEmail(user.email)}
+              selected={selectedEmail === user.email}
+              userEmail={user.email}
+              userName={user.name}
+            />
+          )) ?? <div style={{ height: 180 }} />}
         </div>
         <div className="flex justify-end pt-9">
           <button
             className="rounded-full bg-black p-3 text-white active:opacity-60 disabled:bg-zinc-300"
-            disabled={selectedEmail === null}
+            disabled={selectedEmail === null || isLoading || isMutating}
             onClick={() =>
               typeof selectedEmail === "string" &&
               trigger({ email: selectedEmail })
