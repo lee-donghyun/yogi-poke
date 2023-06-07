16f994c40c1ed99febf006a65d2acc29212dcf4f
Donghyun Lee <leedonghyun022@gmail.com>
2023-06-07 17:20:31 +0900

    fix: import 수정

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
 import "./index.css";
-import { disableBodyScroll } from "body-scroll-lock";
+import { disableBodyScroll } from "body-scroll-lock-upgrade";
 
 disableBodyScroll(document.body);
 
