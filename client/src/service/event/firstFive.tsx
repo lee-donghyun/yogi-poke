d081881cf2e1b4a8d5248cd6aab60aebce409d10
Donghyun Lee <leedonghyun022@gmail.com>
2023-06-09 11:08:14 +0900

    feat: 이메일 자동완성 개선

--- client/src/service/event/firstFive.tsx
+++ client/src/service/event/firstFive.tsx
@@ -1,3 +1,4 @@
+import { useUser } from "../../component/Auth";
 import { createDraggableSheet } from "../../component/StackedLayerProvider";
 import { usePoke } from "../../hook/usePoke";
 
@@ -19,48 +20,54 @@ const CloseIcon = () => (
 );
 
 const HIDE_LAYER_PERSIST_KEY = "EVENT_LAYER_FIRST_5";
-const EventLayer = createDraggableSheet(({ close }) => (
-  <div className="p-5">
-    <div className="flex items-center justify-between">
-      <p className="text-xl font-medium">스타벅스 기프티콘 이벤트 ☕️</p>
-      <button className="text-zinc-500" onClick={close}>
-        <CloseIcon />
-      </button>
+const EventLayer = createDraggableSheet(({ close }) => {
+  const { myInfo } = useUser();
+  return (
+    <div className="p-5">
+      <div className="flex items-center justify-between">
+        <p className="text-xl font-medium">스타벅스 기프티콘 이벤트 ☕️</p>
+        <button className="text-zinc-500" onClick={close}>
+          <CloseIcon />
+        </button>
+      </div>
+      <p className="mt-5 break-keep text-zinc-900">
+        5명을 콕! 찌른 회원님께
+        <br />
+        <span className="animate-pulse font-medium text-green-900">
+          스타벅스 기프티콘
+        </span>
+        을 드립니다. <br />
+        아래 링크에서 회원님의 휴대폰 번호를 입력해주세요!
+      </p>
+      <p className="mt-1.5 text-sm text-zinc-300">
+        개인정보는 이벤트가 종료된 후 파기됩니다.
+      </p>
+      <div className="mt-7">
+        <button
+          className="block w-full rounded-xl bg-black p-4 text-white duration-300 active:opacity-60"
+          onClick={() => {
+            window.open(
+              "https://docs.google.com/forms/d/e/1FAIpQLSeNaE6jbhJgnzPi7nNp04uTmAtZxHeuZqG6Dg4NJNxnTeL_Wg/viewform?usp=pp_url&entry.29515224=" +
+                myInfo?.email
+            );
+            close();
+          }}
+        >
+          응모하러 가기
+        </button>
+        <button
+          className="mt-4 block w-full rounded-xl bg-zinc-50 p-2 text-black duration-300 active:opacity-60"
+          onClick={() => {
+            localStorage.setItem(HIDE_LAYER_PERSIST_KEY, "1");
+            close();
+          }}
+        >
+          다시 보지 않기
+        </button>
+      </div>
     </div>
-    <p className="mt-5 break-keep text-zinc-900">
-      5명을 콕! 찌른 회원님께
-      <br />
-      <span className="animate-pulse font-medium text-green-900">
-        스타벅스 기프티콘
-      </span>
-      을 드립니다. <br />
-      아래 링크에서 회원님의 휴대폰 번호를 입력해주세요!
-    </p>
-    <p className="mt-1.5 text-sm text-zinc-300">
-      개인정보는 이벤트가 종료된 후 파기됩니다.
-    </p>
-    <div className="mt-7">
-      <button
-        className="block w-full rounded-xl bg-black p-4 text-white duration-300 active:opacity-60"
-        onClick={() => {
-          window.open("https://docs.google.com/forms/d/e/1FAIpQLSeNaE6jbhJgnzPi7nNp04uTmAtZxHeuZqG6Dg4NJNxnTeL_Wg/viewform?usp=pp_url&entry.29515224="+"donghyun_");
-          close();
-        }}
-      >
-        응모하러 가기
-      </button>
-      <button
-        className="mt-4 block w-full rounded-xl bg-zinc-50 p-2 text-black duration-300 active:opacity-60"
-        onClick={() => {
-          localStorage.setItem(HIDE_LAYER_PERSIST_KEY, "1");
-          close();
-        }}
-      >
-        다시 보지 않기
-      </button>
-    </div>
-  </div>
-));
+  );
+});
 export const eventPokeProps: Parameters<typeof usePoke>[0] = {
   onSuccess: ({ stack, push, meta: { myInfo, email } }) => {
     if (
