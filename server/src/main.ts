8455be051798f2124933465f5a75b4b3200adb12
Donghyun Lee <leedonghyun022@gmail.com>
2024-02-25 18:18:21 +0900

    feat: nestjs 설치

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,26 +1,8 @@
-import fastify from 'fastify';
-import { router } from './route/router';
-import { authPlugin } from './plugin/auth';
-import fastifySwagger from '@fastify/swagger';
-import fastifySwaggerUi from '@fastify/swagger-ui';
-import cors from '@fastify/cors';
-import { Error2 } from './utils/error';
-import fastifyMultipart from '@fastify/multipart';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
-const app = fastify({ logger: true });
-
-app.register(fastifySwagger);
-app.register(fastifySwaggerUi);
-app.register(cors);
-app.register(fastifyMultipart);
-app.register(router);
-app.register(authPlugin);
-
-app.setErrorHandler(async (error: Error2, _, reply) => {
-  const { statusCode = 500, ...rest } = error;
-  reply.status(statusCode);
-  console.error(error);
-  return rest;
-});
-
-app.listen({ port: Number(process.env.PORT ?? 8080), host: '0.0.0.0' });
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();
