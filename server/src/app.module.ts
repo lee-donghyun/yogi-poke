9d7e3c6f205cfd3a400edc1601e9d83afa487814
Donghyun Lee <leedonghyun022@gmail.com>
2024-04-24 16:07:40 +0900

    feat: 모듈 정리

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,15 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MateModule } from './mate/mate.module';
-import { UtilService } from './util/date.service';
-import { PushModule } from './push/push.module';
-import { PushService } from './push/push.service';
-import { DateUtilModule } from './date-util/date-util.module';
-import { UtilModule } from './util/util.module';
-import { UtilDateService } from './util.date/util.date.service';
-import { UtilService } from './util/date.service';
 
 @Module({
-  imports: [MateModule, UtilModule, DateUtilModule, PushModule],
-  providers: [UtilService, UtilDateService, PushService],
+  imports: [MateModule],
 })
 export class AppModule {}
