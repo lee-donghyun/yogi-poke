61ae91d72cff7c0aff9449a701a5bf267cab8d75
Donghyun Lee <leedonghyun022@gmail.com>
2024-04-24 12:41:57 +0900

    feat: post relation api 추가

---
+++ server/src/auth/auth.decorator.ts
@@ -0,0 +1,9 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { JwtPayload } from './auth.interface';
+
+export const User = createParamDecorator(
+  (data: unknown, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest();
+    return request.userInfo as JwtPayload;
+  },
+);
