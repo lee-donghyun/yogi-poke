3a1fdba01101e8eb7604d9de39e1b5c80381f8d3
Donghyun Lee <leedonghyun022@gmail.com>
2024-04-24 15:40:02 +0900

    feat: 모듈 구조 정리

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,15 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MateModule } from './mate/mate.module';
-import { AuthService } from './auth/auth.service';
-import { UserService } from './user/user.service';
-import { AuthModule } from './auth/auth.module';
-import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [MateModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService, AuthService, UserService, PrismaService],
+  imports: [MateModule],
 })
 export class AppModule {}
