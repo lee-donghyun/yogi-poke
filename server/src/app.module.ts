8c47e818d65d55b0b8335feb51862bdef17b7d23
Donghyun Lee <leedonghyun022@gmail.com>
2024-05-22 08:33:31 +0900

    feat: .env 분기 추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MateModule } from './mate/mate.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MateModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.dev', '.env.prod'],
+    }),
+    MateModule,
+  ],
 })
 export class AppModule {}
