634e76f9bb6fe3d5746af0b6712099ef74f50d8d
Donghyun Lee <leedonghyun022@gmail.com>
2024-06-25 15:54:21 +0900

    feat: 구성 문서 개선

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,17 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MateModule } from './mate/mate.module';
-import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { UtilModule } from './util/util.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: ['.env.dev', '.env.prod'],
-    }),
-    UserModule,
-    MateModule,
-    UtilModule,
-  ],
+  imports: [UserModule, MateModule, UtilModule],
 })
 export class AppModule {}
