added8ef81b907258e544d692f1f475cb411d2d7
Donghyun Lee <leedonghyun022@gmail.com>
2024-06-25 13:21:38 +0900

    feat: file util api 구현

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MateModule } from './mate/mate.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { UtilModule } from './util/util.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { UserModule } from './user/user.module';
     }),
     MateModule,
     UserModule,
+    UtilModule,
   ],
 })
 export class AppModule {}
---
+++ server/src/util/file-util/file-util.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Client as MinioClient } from 'minio';
+
+@Injectable()
+export class FileUtilService implements OnModuleInit {
+  private minioClient: MinioClient;
+  async onModuleInit() {
+    this.minioClient = new MinioClient({
+      endPoint: 'http://127.0.0.1:9000',
+      port: 9000,
+      accessKey: 'cGaxeb4LIXbEYXWeiGDW',
+      secretKey: 'CnIDRgQtgpgjVy1qEpiA9JP0QdbtpxZ27saB2yCa',
+    });
+  }
+  async uploadImageAndGetUrl(file: Express.Multer.File) {
+    await this.minioClient.putObject(
+      process.env.ASSET_BUCKET_ID,
+      file.filename,
+      file.buffer,
+    );
+    return `${process.env.API_URL}/image/${file.filename}`;
+  }
+
+  async getImage(name: string) {
+    const object = await this.minioClient.getObject(
+      process.env.ASSET_BUCKET_ID,
+      name,
+    );
+    return object;
+  }
+}
