1f5aaa2bdfdff7e180a6a2f9796b7b4196831ace
Donghyun Lee <leedonghyun022@gmail.com>
2024-06-25 16:07:50 +0900

    feat: 업로드, 다운로드 실패 예외처리

--- server/src/util/file-util/file-util.service.ts
+++ server/src/util/file-util/file-util.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  OnModuleInit,
+} from '@nestjs/common';
 import { Client as MinioClient } from 'minio';
 
 @Injectable()
@@ -9,24 +14,35 @@ export class FileUtilService implements OnModuleInit {
       endPoint: '127.0.0.1',
       port: 9000,
       useSSL: false,
-      accessKey: 'cGaxeb4LIXbEYXWeiGDW',
-      secretKey: 'CnIDRgQtgpgjVy1qEpiA9JP0QdbtpxZ27saB2yCa',
+      accessKey: 'pX7CzzeEyaKgk7JrkcDr',
+      secretKey: '5MBMQr5tsnohsnRVHDEteXnze6GxunKXZpGol039',
     });
   }
   async uploadImageAndGetUrl(file: Express.Multer.File) {
-    await this.minioClient.putObject(
-      process.env.ASSET_BUCKET_ID,
-      file.filename,
-      file.buffer,
-    );
-    return `${process.env.SERVER_URL}/util/image/${file.filename}`;
+    try {
+      await this.minioClient.putObject(
+        process.env.ASSET_BUCKET_ID,
+        file.filename,
+        file.buffer,
+      );
+      return `${process.env.SERVER_URL}/util/image/${file.filename}`;
+    } catch {
+      throw new HttpException(
+        'Failed to upload image',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   async getImage(name: string) {
-    const object = await this.minioClient.getObject(
-      process.env.ASSET_BUCKET_ID,
-      name,
-    );
-    return object;
+    try {
+      const object = await this.minioClient.getObject(
+        process.env.ASSET_BUCKET_ID,
+        name,
+      );
+      return object;
+    } catch {
+      throw new HttpException('No Such File', HttpStatus.NOT_FOUND);
+    }
   }
 }
