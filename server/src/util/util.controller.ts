d85dffe0f6de21b0066b547df7cb0cff51458803
Donghyun Lee <leedonghyun022@gmail.com>
2024-06-25 13:21:38 +0900

    feat: file util api 구현

---
+++ server/src/util/util.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Controller,
+  FileTypeValidator,
+  Get,
+  MaxFileSizeValidator,
+  Param,
+  ParseFilePipe,
+  Post,
+  StreamableFile,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { FileUtilService } from './file-util/file-util.service';
+import { JwtPayload } from 'src/auth/auth.interface';
+import { User } from 'src/auth/auth.decorator';
+
+@UseGuards(AuthGuard)
+@Controller('util')
+export class UtilController {
+  constructor(private readonly fileUtilService: FileUtilService) {}
+  @UseInterceptors(FileInterceptor('file'))
+  @Post('/image')
+  async uploadImage(
+    @User() user: JwtPayload,
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 4_000_000 }),
+          new FileTypeValidator({ fileType: 'image/*' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    file.filename = `${user.email}-${Date.now()}`;
+    return this.fileUtilService.uploadImageAndGetUrl(file);
+  }
+
+  @Get('/image/:filename')
+  async getImage(@Param('filename') filename: string) {
+    const file = await this.fileUtilService.getImage(filename);
+    return new StreamableFile(file);
+  }
+}
--- server/src/util/util.module.ts
+++ server/src/util/util.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DateUtilService } from './date-util/date-util.service';
+import { UtilController } from './util.controller';
+import { FileUtilService } from './file-util/file-util.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  providers: [DateUtilService],
+  imports: [AuthModule],
+  providers: [DateUtilService, FileUtilService],
   exports: [DateUtilService],
+  controllers: [UtilController],
 })
 export class UtilModule {}
