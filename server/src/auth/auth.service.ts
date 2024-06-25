b2dc6ccadd6444c1157609c6b245f98d08f83bba
Donghyun Lee <leedonghyun022@gmail.com>
2024-06-25 14:40:37 +0900

    fix: AuthGuard 검증 로직 버그

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,29 +1,41 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  OnModuleInit,
+} from '@nestjs/common';
+
 import { verify, sign } from 'jsonwebtoken';
 import { JwtPayload } from './auth.interface';
 
 @Injectable()
-export class AuthService {
-  static JWT_SECRET = process.env.JWT_SECRET || '';
+export class AuthService implements OnModuleInit {
+  private JWT_SECRET: string;
+  onModuleInit() {
+    this.JWT_SECRET = process.env.JWT_SECRET;
+  }
   validateRequest(request: any): boolean {
     request.user = null;
     const token = request.headers.authorization;
-    if (!token) return;
+
+    if (!token) return false;
     try {
       const user = this.verifyUserToken(token);
       request.user = user;
+      return true;
     } catch {
       request.user = null;
+      return false;
     }
   }
   verifyUserToken(token: string) {
     try {
-      return verify(token, AuthService.JWT_SECRET) as JwtPayload;
+      return verify(token, this.JWT_SECRET) as JwtPayload;
     } catch {
       throw new HttpException('Invalid token', HttpStatus.UNAUTHORIZED);
     }
   }
   async createUserToken(user: JwtPayload) {
-    return sign(user, AuthService.JWT_SECRET);
+    return sign(user, this.JWT_SECRET);
   }
 }
