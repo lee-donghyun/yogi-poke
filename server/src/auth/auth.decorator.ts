ba479e6ac39949387fae89401582f55aa55a4909
Donghyun Lee <leedonghyun022@gmail.com>
2024-06-25 14:43:26 +0900

    fix: jwt payload 버그

--- server/src/auth/auth.decorator.ts
+++ server/src/auth/auth.decorator.ts
@@ -4,6 +4,6 @@ import { JwtPayload } from './auth.interface';
 export const User = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
-    return request.userInfo as JwtPayload;
+    return request.user as JwtPayload;
   },
 );
