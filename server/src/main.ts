43eb0b58eb8c420188b24bba4471e8865a568fca
Donghyun Lee <leedonghyun022@gmail.com>
2024-07-03 21:59:45 +0900

    feat: https 인증서 연결

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    cert: readFileSync(join(__dirname, '../certification/fullchain.pem')),
+    key: readFileSync(join(__dirname, '../certification/privkey.pem')),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.enableCors({ origin: process.env.CLIENT_URL });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(process.env.PORT ?? 8080);
