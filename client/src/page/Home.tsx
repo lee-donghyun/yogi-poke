08d127096a2c433d16b1c7fc1c4bf339d70af14e
Donghyun Lee <leedonghyun022@gmail.com>
2024-07-05 16:40:52 +0900

    feat: 홈 화면 디자인 개선

--- client/src/page/Home.tsx
+++ client/src/page/Home.tsx
@@ -1,7 +1,6 @@
 import { Link, useRouter } from "router2";
 
 import { useUser } from "../component/Auth";
-import { Introduction } from "./Introduction";
 
 export const Home = () => {
   const { navigate, params } = useRouter();
@@ -11,9 +10,9 @@ export const Home = () => {
     navigate({ pathname: "/search" }, { replace: true });
   }
   return (
-    <div className="min-h-screen">
-      <Introduction />
-      <div className="flex flex-col gap-5 p-5">
+    <div>
+      <img alt="" className="mx-auto mt-20 size-60" src="/asset/icon.jpg" />
+      <div className="fixed inset-x-0 bottom-0 flex flex-col gap-5 p-5">
         <Link replace pathname="/register" query={params}>
           <button className="block w-full rounded-full bg-black p-4 text-white duration-300 active:opacity-60">
             회원가입
