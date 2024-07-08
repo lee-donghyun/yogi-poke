f0d34367c3b81a922c15320e625e9ff57abd13b9
Donghyun Lee <leedonghyun022@gmail.com>
2024-07-09 00:45:37 +0900

    feat: 서버 인증서 제거

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,17 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { readFileSync } from 'fs';
-import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions: {
-      cert: readFileSync(join(__dirname, '../certification/cert.pem')),
-      ca: readFileSync(join(__dirname, '../certification/chain.pem')),
-      key: readFileSync(join(__dirname, '../certification/privkey.pem')),
-    },
-  });
+  const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: process.env.CLIENT_URL });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(process.env.PORT ?? 8080);
