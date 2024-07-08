6a5db4181ec972f79bb96ed1246ab3aba1e4f70f
Donghyun Lee <leedonghyun022@gmail.com>
2024-07-08 17:10:13 +0900

    feat: ip 검증 로직 제거 : (github에서 제공하지 않음)

--- ci-server/app.py
+++ ci-server/app.py
@@ -9,18 +9,6 @@ client = docker.from_env()
 client.login(username="donghyunlee022", password="s/-D74mVz4gjw#t")
 
 
-def is_valid_ip(ip):
-    def get_github_actions_ip_ranges():
-        response = requests.get("https://api.github.com/meta")
-        response.raise_for_status()
-        return response.json()["actions"]
-
-    for ip_range in get_github_actions_ip_ranges():
-        if ipaddress.ip_address(ip) in ipaddress.ip_network(ip_range):
-            return True
-    return False
-
-
 def is_valid_token(token):
     SECRET_KEY = "very_secret_key"
     return token == SECRET_KEY
@@ -28,10 +16,6 @@ def is_valid_token(token):
 
 @app.before_request
 def before_request():
-    client_ip = request.remote_addr
-    if not is_valid_ip(client_ip):
-        return jsonify({"error": "Invalid IP address"}), 403
-
     token = request.headers.get("Authorization")
     if token is None or not is_valid_token(token):
         return jsonify({"error": "Invalid or missing token"}), 403
