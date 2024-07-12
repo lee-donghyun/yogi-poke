fbd184100d828b4b755d734ef180f22b68c1efc2
Donghyun Lee <leedonghyun022@gmail.com>
2024-07-12 22:18:03 +0900

    feat: 사용자 차단 api/ui

--- client/src/page/User.tsx
+++ client/src/page/User.tsx
@@ -1,6 +1,7 @@
 import dayjs, { isDayjs } from "dayjs";
 import { useRouter } from "router2";
 import useSWR from "swr";
+import useSWRMutation from "swr/mutation";
 
 import { useUser } from "../component/Auth";
 import { Block } from "../component/icon/Block";
@@ -11,6 +12,7 @@ import { Stat } from "../component/Stat";
 import { Timer } from "../component/Timer";
 import { useLocalStorage } from "../hook/useLocalStorage";
 import { usePoke } from "../hook/usePoke";
+import { yogiPokeApi } from "../service/api";
 import { LIKE_PERSIST_KEY } from "../service/const";
 
 export const DAY_IN_UNIX = 1000 * 60 * 60 * 24;
@@ -48,6 +50,15 @@ export const User = () => {
     mutate: mutateUserPoke,
   } = useSWR<UserPokeData[]>([`/mate/poke/${userEmail}`, { limit: 1 }]);
 
+  const { trigger: triggerBlock, isMutating: isBlockLoading } = useSWRMutation(
+    `/user/relation/${userEmail}`,
+    (api) =>
+      yogiPokeApi.patch(api, { isAccepted: false }).then(() => {
+        push({ content: "사용자를 차단했습니다." });
+        history.back();
+      }),
+  );
+
   const [likes, setLikes] = useLocalStorage<number[]>(LIKE_PERSIST_KEY, []);
   const isLiked = typeof data?.id === "number" && likes.includes(data.id);
   const lastPoked =
@@ -64,9 +75,10 @@ export const User = () => {
           <button
             key="block"
             className="text-zinc-400 active:opacity-60"
+            disabled={isBlockLoading}
             type="button"
             onClick={() => {
-              push({ content: "사용자를 차단하려면 관리자에게 문의하세요." });
+              void triggerBlock();
             }}
           >
             <Block />
