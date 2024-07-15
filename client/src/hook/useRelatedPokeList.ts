a73f53396d56d59f59f09300fdf2cd4d62dfca56
Donghyun Lee <leedonghyun022@gmail.com>
2024-07-15 13:47:58 +0900

    feat: poke list 훅 분리

---
+++ client/src/hook/useRelatedPokeList.ts
@@ -0,0 +1,36 @@
+import { useCallback, useRef } from "react";
+import useSWRInfinite from "swr/infinite";
+
+import { Poke } from "../service/dataType";
+import { useIntersectionObserver } from "./useIntersectionObserver";
+
+const POKE_LIST_LIMIT = 20;
+
+export const useRelatedPokeList = () => {
+  const { data, setSize, error, isLoading, mutate } = useSWRInfinite<
+    Poke[],
+    unknown
+  >((index, previous) =>
+    index === 0 || (previous && previous.length === POKE_LIST_LIMIT)
+      ? ["/mate/poke", { limit: POKE_LIST_LIMIT, page: index + 1 }]
+      : null,
+  );
+  const loadMore = useCallback(
+    () => !isLoading && !error && void setSize((prev) => prev + 1),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [isLoading, setSize, !!error],
+  );
+  const intersectorRef = useIntersectionObserver(loadMore);
+  const prevData = useRef(data);
+
+  const isFreshData = (pageIndex: number) =>
+    pageIndex + 1 > (prevData.current?.length ?? 0);
+
+  return {
+    data,
+    error,
+    mutate,
+    intersectorRef,
+    isFreshData,
+  };
+};
