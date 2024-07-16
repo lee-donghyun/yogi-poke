4dfc7cd77452b220b3c90da757895bd111de8eb9
Donghyun Lee <leedonghyun022@gmail.com>
2024-07-16 15:56:39 +0900

    refactor: relation service 개선

--- server/src/relation/relation.service.ts
+++ server/src/relation/relation.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class RelationService {
   constructor(private db: PrismaService) {}
+  async getRelation(fromUserId: number, toUserId: number) {
+    const relation = await this.db.relation.findFirst({
+      where: { fromUserId, toUserId },
+    });
+    return relation;
+  }
+
+  async createRelation(fromUserId: number, toUserId: number) {
+    const relation = await this.getRelation(fromUserId, toUserId);
+    if (relation !== null) {
+      throw new HttpException('Already has relation', HttpStatus.CONFLICT);
+    }
+    const made = await this.db.relation.create({
+      data: { fromUserId, toUserId },
+    });
+    return made;
+  }
   async updateUserAcception(
     isAccepted: boolean,
     {
