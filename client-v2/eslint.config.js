88e031f840dc6d25eb1ae47e49aa8770911f0513
Donghyun Lee <leedonghyun022@gmail.com>
2024-07-29 18:13:26 +0900

    feat: 스콥 통일, react 룰 추가

--- client-v2/eslint.config.js
+++ client-v2/eslint.config.js
@@ -1,20 +1,34 @@
 import js from "@eslint/js";
-import perfectionist from "eslint-plugin-perfectionist";
+import React from "eslint-plugin-react";
 import solid from "eslint-plugin-solid";
 import ts from "typescript-eslint";
+import perfectionist from "eslint-plugin-perfectionist";
+
+const scope = (config) => ({
+  ...config,
+  files: ["src/**/*.{ts,tsx}"],
+});
 
 export default [
   js.configs.recommended,
   perfectionist.configs["recommended-natural"],
-  solid.configs["flat/typescript"],
-  ...ts.configs.recommended,
   {
-    files: ["src/**/*.{ts,tsx}"],
-    languageOptions: {
-      parser: ts.parser,
-      parserOptions: {
-        project: "tsconfig.app.json",
-      },
+    name: "jsx rules from eslint-plugin-react",
+    plugins: { react: React },
+    rules: {
+      "react/jsx-curly-brace-presence": ["warn", "never"],
     },
   },
-];
+  solid.configs["flat/typescript"],
+  ...[ts.configs.recommendedTypeChecked, ts.configs.stylisticTypeChecked]
+    .flat()
+    .map((config) => ({
+      ...config,
+      languageOptions: {
+        parser: ts.parser,
+        parserOptions: {
+          project: "tsconfig.app.json",
+        },
+      },
+    })),
+].map(scope);
--- client-v2/src/App.tsx
+++ client-v2/src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div class="dfd">
       <h1>여기에서 무언가 보여드리겠습니다.</h1>
     </div>
   );
