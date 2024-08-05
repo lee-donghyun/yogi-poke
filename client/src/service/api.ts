4d001e996c2511e02a26618d5ee6f47a7a3791c2
Donghyun Lee <leedonghyun022@gmail.com>
2024-08-05 17:19:36 +0900

    feat: ky 인스턴스 추가

--- client/src/service/api.ts
+++ client/src/service/api.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
+import ky from "ky";
 
 export const yogiPokeApi = axios.create({
   baseURL: import.meta.env.VITE_YOGI_POKE_API_URL,
+});
+
+export const client = ky.create({
+  prefixUrl: import.meta.env.VITE_YOGI_POKE_API_URL,
 });
