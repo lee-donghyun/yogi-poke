89afaa31ba7ed9f2d8ac935e127ae2ff699b8e1d
Donghyun Lee <leedonghyun022@gmail.com>
2024-08-09 12:07:36 +0900

    feat: 그림 다시보기 컴포넌트 작성

---
+++ client/src/ui/domain/ShowDrawing.tsx
@@ -0,0 +1,41 @@
+import { lazy, Suspense } from "react";
+
+import { useDomSize } from "../../hook/base/useDomSize";
+import { type Line } from "../base/Canvas";
+import { createDraggableSheet } from "../provider/StackedLayerProvider";
+
+const CanvasRenderer = lazy(() =>
+  import("../base/CanvasRenderer").then((module) => ({
+    default: module.CanvasRenderer,
+  })),
+);
+
+export const ShowDrawing = createDraggableSheet<{
+  lines: Line[];
+  name: string;
+}>(({ context }) => {
+  const {
+    domRef,
+    size: { height, width },
+  } = useDomSize<HTMLDivElement>();
+
+  return (
+    <div className="p-6">
+      <p className="text-lg font-semibold text-zinc-800">
+        {context.name}님이 보낸 그림
+      </p>
+      <div className="h-6"></div>
+      <div
+        className="aspect-square w-full"
+        data-allow-touch-move-on-stacked-layer
+        ref={domRef}
+      >
+        <Suspense
+          fallback={<div className="size-full rounded-2xl bg-black"></div>}
+        >
+          <CanvasRenderer height={height} lines={context.lines} width={width} />
+        </Suspense>
+      </div>
+    </div>
+  );
+});
