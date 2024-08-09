b654d75dd2d2ad4450932db1fbb54b9143ee1af8
Donghyun Lee <leedonghyun022@gmail.com>
2024-08-09 12:07:36 +0900

    feat: 그림 다시보기 컴포넌트 작성

---
+++ client/src/ui/base/CanvasRenderer.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useMemo, useState } from "react";
+import { Line as KonvaLine, Layer, Stage } from "react-konva";
+
+import { type Line } from "./Canvas";
+
+interface CanvasRendererProps {
+  height: number;
+  lines: Line[];
+  width: number;
+}
+
+export const CanvasRenderer = ({
+  height,
+  lines,
+  width,
+}: CanvasRendererProps) => {
+  const [drawCount, setDrawCount] = useState(0);
+
+  const pointsCount = useMemo(
+    () => lines.reduce((acc, line) => acc + line.points.length, 0) / 4,
+    [lines],
+  );
+
+  useEffect(() => {
+    if (drawCount < pointsCount) {
+      const timer = setTimeout(
+        () => {
+          setDrawCount((p) => p + 1);
+        },
+        // 그리기 시작하기 전에 딜레이를 준다.
+        drawCount === 0 ? 600 : 25,
+      );
+      return () => clearTimeout(timer);
+    }
+  }, [drawCount, pointsCount]);
+
+  const draws = lines.reduce(
+    ({ lines, top }, line) => {
+      return {
+        lines: [...lines, { ...line, points: line.points.slice(0, top * 4) }],
+        top: Math.max(0, top - Math.min(line.points.length / 4, top)),
+      };
+    },
+    { lines: [], top: drawCount } as { lines: Line[]; top: number },
+  );
+
+  return (
+    <Stage
+      className="overflow-hidden rounded-2xl bg-black"
+      height={height}
+      width={width}
+    >
+      <Layer>
+        {draws.lines.map((line) => (
+          <KonvaLine
+            bezier
+            key={line.id}
+            lineCap="round"
+            lineJoin="round"
+            points={line.points}
+            stroke={line.color}
+          />
+        ))}
+      </Layer>
+    </Stage>
+  );
+};
