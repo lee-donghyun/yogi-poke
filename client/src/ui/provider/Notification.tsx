d367659694de51346d5d8a094939ac5747f4b272
Donghyun Lee <leedonghyun022@gmail.com>
2024-08-09 18:05:48 +0900

    chore: sonner lazy loading 제거

--- client/src/ui/provider/Notification.tsx
+++ client/src/ui/provider/Notification.tsx
@@ -1,70 +1,33 @@
-import {
-  createContext,
-  JSX,
-  lazy,
-  Suspense,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
-
-import { noop } from "../../service/util";
+import { createContext, JSX, useContext } from "react";
+import { toast, Toaster } from "sonner";
 
 interface NotificationData {
   content: string;
 }
 
 const notificationContext = createContext<
-  [(notification: NotificationData) => void, () => void]
->([
-  () => {
-    throw new Error(
-      "useNotification hook-domain need to be called in NotificationProvider",
-    );
-  },
-  noop,
-]);
+  (notification: NotificationData) => void
+>(() => {
+  throw new Error(
+    "useNotification hook-domain need to be called in NotificationProvider",
+  );
+});
 
-export const useNotification = () => {
-  const [push, init] = useContext(notificationContext);
-  useEffect(init, [init]);
-  return push;
-};
-
-const ToastProvider = lazy(() =>
-  import("sonner").then((module) => ({ default: module.Toaster })),
-);
+export const useNotification = () => useContext(notificationContext);
 
 export const NotificationProvider = ({
   children,
 }: {
   children: JSX.Element;
 }) => {
-  const [load, setLoad] = useState(false);
-
   const push = ({ content }: NotificationData) => {
-    void import("sonner").then(({ toast }) =>
-      toast("요기콕콕!", { description: content }),
-    );
+    toast("요기콕콕!", { description: content });
   };
 
-  const init = !load
-    ? () => {
-        if (import.meta.env.DEV) {
-          console.warn("NotificationProvider is initialized");
-        }
-        setLoad(true);
-      }
-    : noop;
-
   return (
-    <notificationContext.Provider value={[push, init]}>
+    <notificationContext.Provider value={push}>
       {children}
-      {load && (
-        <Suspense>
-          <ToastProvider closeButton position="top-center" />
-        </Suspense>
-      )}
+      <Toaster closeButton position="top-center" />
     </notificationContext.Provider>
   );
 };
