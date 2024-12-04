63a4aaf2ca0dc61702a25a972bab2149ad65a89d
Donghyun Lee <leedonghyun022@gmail.com>
2024-12-04 17:00:27 +0900

    feat: 확대 가능 개선

--- client/src/ui/base/LocationPuck.tsx
+++ client/src/ui/base/LocationPuck.tsx
@@ -6,14 +6,16 @@
 export const LocationPuck = ({
   confidenceRadius,
 }: {
-  confidenceRadius: number;
+  confidenceRadius?: number;
 }) => {
   return (
     <div className="relative flex items-center justify-center">
-      <div
-        className="absolute rounded-full bg-blue-500 opacity-10 transition-all"
-        style={{ height: confidenceRadius * 2, width: confidenceRadius * 2 }}
-      ></div>
+      {typeof confidenceRadius === "number" && (
+        <div
+          className="absolute rounded-full bg-blue-500 opacity-10 transition-all"
+          style={{ height: confidenceRadius * 2, width: confidenceRadius * 2 }}
+        ></div>
+      )}
       <div className="absolute h-3 w-3 rounded-full bg-blue-500 shadow-lg"></div>
     </div>
   );
