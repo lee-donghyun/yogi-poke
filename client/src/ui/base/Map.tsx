11c586a88f44caabe2b67bcab992d6e5da91a2e9
Donghyun Lee <leedonghyun022@gmail.com>
2024-12-04 16:58:31 +0900

    feat: Map 컴포넌트 개선

--- client/src/ui/base/Map.tsx
+++ client/src/ui/base/Map.tsx
@@ -1,37 +1,45 @@
 import { Map as MapboxMap, Marker } from "react-map-gl";
-import useSWR from "swr";
 import "mapbox-gl/dist/mapbox-gl.css";
 
-export const Map = ({ height, width }: { height: number; width: number }) => {
-  const { data: position } = useSWR(
-    "geolocation",
-    () =>
-      new Promise<GeolocationPosition>((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject);
-      }),
-    {
-      suspense: true,
-    },
-  );
+import { useGeoLocation } from "../../hook/base/useGeolocation";
+import { LocationPuck } from "./LocationPuck";
+
+export const Map = ({
+  height,
+  position,
+  showCurrentPosition,
+  width,
+}: {
+  height: number;
+  position: Pick<GeolocationPosition["coords"], "latitude" | "longitude">;
+  showCurrentPosition?: boolean;
+  width: number;
+}) => {
+  const currentPosition = useGeoLocation({ suspense: true });
 
   return (
-    position && (
+    currentPosition && (
       <MapboxMap
         initialViewState={{
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          zoom: 12,
+          latitude: position.latitude,
+          longitude: position.longitude,
         }}
         mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
         mapStyle="mapbox://styles/mapbox/streets-v12"
         style={{ height, width }}
+        zoom={12}
       >
-        <Marker
-          latitude={position.coords.latitude}
-          longitude={position.coords.longitude}
-        >
+        <Marker latitude={position.latitude} longitude={position.longitude}>
           <span className="text-5xl">📍</span>
         </Marker>
+        {showCurrentPosition && (
+          <Marker
+            latitude={currentPosition.coords.latitude}
+            longitude={currentPosition.coords.longitude}
+          >
+            <LocationPuck confidenceRadius={currentPosition.coords.accuracy} />
+          </Marker>
+        )}
       </MapboxMap>
     )
   );
