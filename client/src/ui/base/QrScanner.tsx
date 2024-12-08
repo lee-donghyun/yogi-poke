364ba57d6b5d87022e6e4eefe0f2dfc1ca600f3a
Donghyun Lee <leedonghyun022@gmail.com>
2024-12-08 22:59:25 +0900

    feat: 직접 DOM 조작 제거

--- client/src/ui/base/QrScanner.tsx
+++ client/src/ui/base/QrScanner.tsx
@@ -1,14 +1,20 @@
 import QrScannerClient from "qr-scanner";
-import { useEffect, useMemo, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { Task } from "../../lib/task";
 
 export const QrScanner = ({
   onScan,
 }: {
+  /**
+   * memo가 된 함수를 전달합니다.
+   */
   onScan: (scanner: QrScannerClient.ScanResult) => void;
 }) => {
   const ref = useRef<HTMLVideoElement>(null);
+
+  const [pulse, setPulse] = useState(true);
+
   useEffect(() => {
     if (ref.current === null) return;
     const scanner = new QrScannerClient(ref.current, onScan, {
@@ -18,18 +24,16 @@ export const QrScanner = ({
 
     const task = new Task();
     task.pipe(() => scanner.start());
-    task.pipe(() => ref.current?.classList.remove("animate-pulse"));
+    task.pipe(() => setPulse(false));
     return () => task.pipe(() => scanner.destroy());
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  return useMemo(
-    () => (
-      // eslint-disable-next-line jsx-a11y/media-has-caption
-      <video
-        className="aspect-square w-full animate-pulse rounded-2xl bg-zinc-100 object-cover"
-        ref={ref}
-      ></video>
-    ),
-    [],
+  }, [onScan]);
+  return (
+    // eslint-disable-next-line jsx-a11y/media-has-caption
+    <video
+      className={`aspect-square w-full rounded-2xl bg-zinc-100 object-cover ${
+        pulse ? "animate-pulse" : ""
+      }`}
+      ref={ref}
+    ></video>
   );
 };
