f6a7915cedcb3253f8b690c5ec9e11d86f359800
Donghyun Lee <leedonghyun022@gmail.com>
2024-12-25 21:44:38 +0900

    chore: 클라이언트 sentry 추가

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,29 @@
+import * as Sentry from "@sentry/react";
 import { disableBodyScroll } from "body-scroll-lock-upgrade";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { App } from "./App.tsx";
 import "./index.css";
+import { App } from "./App.tsx";
+
+Sentry.init({
+  dsn: "https://37e4abc51989cf0249d52d49685e20ae@o4505527670210560.ingest.us.sentry.io/4508528810917888",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  replaysOnErrorSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  tracePropagationTargets: [
+    "localhost",
+    import.meta.env.VITE_YOGI_POKE_API_URL,
+  ],
+  tracesSampleRate: 1.0,
+});
 
 disableBodyScroll(document.body);
 
 const rootElement = document.getElementById("root");
-
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
