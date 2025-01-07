f70e918f1d40d86bdf150daadf52d360ed534723
Donghyun Lee <leedonghyun022@gmail.com>
2025-01-07 21:35:28 +0900

    feat: option api 추가, 스키마 변경

---
+++ server/src/auth/auth.constant.ts
@@ -0,0 +1,5 @@
+export const passKeyConstants = {
+  rpName: '요기콕콕!',
+  rpID: 'localhost',
+  origin: `http://${'localhost'}`,
+};
--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,6 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserService } from 'src/user/user.service';
+import { User } from './auth.decorator';
+import { JwtPayload } from './auth.interface';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -8,4 +11,10 @@ export class AuthController {
     private readonly authService: AuthService,
     private readonly userService: UserService,
   ) {}
+
+  @UseGuards(AuthGuard)
+  @Get('passkey')
+  async getPasskey(@User() user: JwtPayload) {
+    return await this.authService.generatePasskeyRegistrationOptions(user);
+  }
 }
