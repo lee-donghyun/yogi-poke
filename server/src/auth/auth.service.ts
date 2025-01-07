d9319782692d19be428198975e9fcf404ac377da
Donghyun Lee <leedonghyun022@gmail.com>
2025-01-07 21:35:28 +0900

    feat: option api 추가, 스키마 변경

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -2,10 +2,19 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
 import { JwtPayload } from './auth.interface';
 import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import {
+  AuthenticatorTransportFuture,
+  generateRegistrationOptions,
+} from '@simplewebauthn/server';
+import { passKeyConstants } from './auth.constant';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly db: PrismaService,
+  ) {}
   validateRequest(request: any): boolean {
     request.user = null;
     const token = request.headers.authorization;
@@ -29,5 +38,35 @@ export class AuthService {
   }
   createUserToken(user: JwtPayload) {
     return this.jwtService.sign(user) as string;
+  }
+  async generatePasskeyRegistrationOptions(user: JwtPayload) {
+    const userPasskeys = await this.db.passkey.findMany({
+      where: { userId: user.id },
+    });
+
+    const options = await generateRegistrationOptions({
+      rpName: passKeyConstants.rpName,
+      rpID: passKeyConstants.rpID,
+      userName: user.name,
+      attestationType: 'none',
+      excludeCredentials: userPasskeys.map((passkey) => ({
+        id: passkey.id,
+        transports: passkey.transports.split(
+          ',',
+        ) as AuthenticatorTransportFuture[],
+      })),
+      authenticatorSelection: {
+        residentKey: 'preferred',
+        userVerification: 'preferred',
+        authenticatorAttachment: 'platform',
+      },
+    });
+
+    await this.db.user.update({
+      data: { passkeyOptions: JSON.stringify(options) },
+      where: { id: user.id },
+    });
+
+    return options;
   }
 }
