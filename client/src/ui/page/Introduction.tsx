11f6c72c8f9609f068ffc80dd0b0741107b037b7
Donghyun Lee <leedonghyun022@gmail.com>
2025-01-08 13:53:34 +0900

    feat: Introduction 추가

--- client/src/ui/page/Introduction.tsx
+++ client/src/ui/page/Introduction.tsx
@@ -1,3 +1,6 @@
+import { t } from "@lingui/core/macro";
+import { Trans } from "@lingui/react/macro";
+
 import { ArrowUpOnSquare } from "../icon/ArrowUpOnSquare.tsx";
 import { DevicePhoneMobile } from "../icon/DevicePhoneMobile.tsx";
 import { useStackedLayer } from "../provider/StackedLayerProvider.tsx";
@@ -6,6 +9,8 @@ import {
   platform,
   pwaHelpUrl,
 } from "./Introduction.Platform.tsx";
+
+const VERSION = "25.01.08";
 
 export const Introduction = () => {
   const overlay = useStackedLayer();
@@ -19,9 +24,11 @@ export const Introduction = () => {
         />
         <div className="flex flex-1 flex-col justify-between pl-5 pt-2">
           <div>
-            <h1 className="text-xl font-bold">요기콕콕!</h1>
+            <h1 className="text-xl font-bold">
+              <Trans>요기콕콕!</Trans>
+            </h1>
             <p className="pt-1 text-sm text-zinc-600">
-              빠르고 간결한 최신 소셜 미디어
+              <Trans>빠르고 간결한 최신 소셜 미디어</Trans>
             </p>
           </div>
           <div className="flex items-center justify-between">
@@ -32,14 +39,14 @@ export const Introduction = () => {
               }}
               type="button"
             >
-              설치
+              <Trans>설치</Trans>
             </button>
             {typeof navigator?.share === "function" && (
               <button
                 className="-mb-1 p-1 text-blue-500 active:opacity-60"
                 onClick={() => {
                   void navigator.share({
-                    title: "요기콕콕!",
+                    title: t`요기콕콕!`,
                     url: "https://yogi-poke.vercel.app?mtag=1",
                   });
                 }}
@@ -52,14 +59,20 @@ export const Introduction = () => {
         </div>
       </div>
       <div className="mx-6 mt-4 border-t pt-3">
-        <h2 className="text-lg font-bold text-zinc-900">새로운 소식</h2>
-        <p className="pt-1 text-xs text-zinc-500">버전 24.08.09</p>
+        <h2 className="text-lg font-bold text-zinc-900">
+          <Trans>새로운 소식</Trans>
+        </h2>
+        <p className="pt-1 text-xs text-zinc-500">
+          <Trans>버전 {VERSION}</Trans>
+        </p>
         <p className="pt-2 text-sm text-zinc-600">
-          이제 그림을 그려서 콕! 찌를 수 있습니다.
+          <Trans>이제 그림을 그려서 콕! 찌를 수 있습니다.</Trans>
         </p>
       </div>
       <div className="mx-5 mt-8 border-t pt-3">
-        <h2 className="text-lg font-bold text-zinc-900">미리보기</h2>
+        <h2 className="text-lg font-bold text-zinc-900">
+          <Trans>미리보기</Trans>
+        </h2>
       </div>
       <div className="flex max-w-full snap-x snap-mandatory gap-3 overflow-x-scroll scroll-smooth px-5 pt-2">
         {[
@@ -86,7 +99,9 @@ export const Introduction = () => {
         target="_blank"
       >
         <DevicePhoneMobile />
-        <p className="pl-1 text-sm">{platform}를 위한 웹 어플리케이션</p>
+        <p className="pl-1 text-sm">
+          <Trans>{platform}를 위한 웹 어플리케이션</Trans>
+        </p>
       </a>
     </div>
   );
