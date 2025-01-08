d12b08a2d98e772d3fb75cb00cbbd31147c42191
Donghyun Lee <leedonghyun022@gmail.com>
2025-01-08 20:18:02 +0900

    feat: pwa provider 번역 추가

--- client/src/ui/page/Introduction.Platform.tsx
+++ client/src/ui/page/Introduction.Platform.tsx
@@ -1,3 +1,5 @@
+import { Trans } from "@lingui/react/macro";
+
 import { switch_ } from "../../lib/expify.ts";
 import { ArrowUpOnSquare } from "../icon/ArrowUpOnSquare.tsx";
 import { createDraggableSheet } from "../provider/StackedLayerProvider.tsx";
@@ -5,7 +7,7 @@ import { createDraggableSheet } from "../provider/StackedLayerProvider.tsx";
 enum Platform {
   ANDROID = "Android",
   IOS = "iOS",
-  OTHERS = "모바일",
+  OTHERS = "Mobile",
 }
 
 export const platform = navigator.userAgent.includes("Android")
@@ -36,34 +38,38 @@ const iosInstruction = () => (
     data-allow-touch-move-on-stacked-layer
   >
     <h3 className="font-semibold text-zinc-800">
-      홈 화면에 요기콕콕! 설치하기
+      <Trans>홈 화면에 요기콕콕! 설치하기</Trans>
     </h3>
     <p className="pt-4 text-sm text-zinc-600">
-      iPhone 홈 화면에 요기콕콕!을 추가하여 어플리케이션을 간편하게 설치할 수
-      있습니다.
+      <Trans>
+        iPhone 홈 화면에 요기콕콕!을 추가하여 어플리케이션을 간편하게 설치할 수
+        있습니다.
+      </Trans>
     </p>
     <ol className="list-decimal pl-5 pt-3 text-sm text-zinc-600">
-      <li>
-        <span className="font-semibold">Safari</span> 브라우저를 열고{" "}
-        <a
-          className="text-blue-500"
-          href="https://yogi-poke.vercel.app?mtag=1"
-          rel="noreferrer"
-          target="_blank"
-        >
-          요기콕콕! 웹 사이트
-        </a>
-        로 이동합니다.
-      </li>
-      <li>
-        웹 사이트를 보는 동안 메뉴 막대에서{" "}
-        <ArrowUpOnSquare className="inline-block size-5 text-blue-500" />을
-        탭하십시오.
-      </li>
-      <li className="mt-2">
-        옵션 목록을 아래로 스크롤한 다음,{" "}
-        <span className="font-semibold">홈 화면에 추가</span>를 탭하십시오.
-      </li>
+      <Trans>
+        <li>
+          <span className="font-semibold">Safari</span> 브라우저를 열고{" "}
+          <a
+            className="text-blue-500"
+            href="https://yogi-poke.vercel.app?mtag=1"
+            rel="noreferrer"
+            target="_blank"
+          >
+            요기콕콕! 웹 사이트
+          </a>
+          로 이동합니다.
+        </li>
+        <li>
+          웹 사이트를 보는 동안 메뉴 막대에서{" "}
+          <ArrowUpOnSquare className="inline-block size-5 text-blue-500" />을
+          탭하십시오.
+        </li>
+        <li className="mt-2">
+          옵션 목록을 아래로 스크롤한 다음,{" "}
+          <span className="font-semibold">홈 화면에 추가</span>를 탭하십시오.
+        </li>
+      </Trans>
     </ol>
     <img
       alt="아이폰 홈 화면에 추가 미리보기"
@@ -76,18 +82,22 @@ const iosInstruction = () => (
 const androidInstruction = () => (
   <div className="p-5 pb-32">
     <h3 className="font-semibold text-zinc-800">
-      홈 화면에 요기콕콕! 설치하기
+      <Trans>홈 화면에 요기콕콕! 설치하기</Trans>
     </h3>
     <p className="pt-4 text-sm text-zinc-600">
-      Android 기기 홈 화면에 요기콕콕!을 추가하여 어플리케이션을 간편하게 설치할
-      수 있습니다.
+      <Trans>
+        Android 기기 홈 화면에 요기콕콕!을 추가하여 어플리케이션을 간편하게
+        설치할 수 있습니다.
+      </Trans>
     </p>
     <ol className="list-decimal pl-5 pt-3 text-sm text-zinc-600">
-      <li>
-        주소 표시줄 오른쪽에서 더보기 {">"} 홈 화면에 추가 {">"} 설치를
-        탭합니다.
-      </li>
-      <li className="mt-2">화면에 표시된 안내를 따릅니다.</li>
+      <Trans>
+        <li>
+          주소 표시줄 오른쪽에서 더보기 {">"} 홈 화면에 추가 {">"} 설치를
+          탭합니다.
+        </li>
+        <li className="mt-2">화면에 표시된 안내를 따릅니다.</li>
+      </Trans>
     </ol>
   </div>
 );
