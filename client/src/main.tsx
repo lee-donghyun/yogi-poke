1b158ce41c9da758ef103118ae568611b8a4a013
Donghyun Lee <leedonghyun022@gmail.com>
2025-01-17 18:06:51 +0900

    feat: body-scroll 제거

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,8 @@
-import { disableBodyScroll } from "body-scroll-lock-upgrade";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
 import { App } from "./App.tsx";
-
-disableBodyScroll(document.body);
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
