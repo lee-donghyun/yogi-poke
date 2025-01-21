6e25e3333d5e6bc7a8e7ea5225b326359cc97e95
Donghyun Lee <leedonghyun022@gmail.com>
2025-01-21 16:58:13 +0900

    feat: 네비게이션 ui 개선

--- client/src/ui/domain/DomainBottomNavigation.tsx
+++ client/src/ui/domain/DomainBottomNavigation.tsx
@@ -5,10 +5,22 @@ import { Search } from "../icon/Search.tsx";
 import { Star } from "../icon/Star.tsx";
 import { useUser } from "../provider/Auth.tsx";
 
+enum Path {
+  LIKE = "/like",
+  MY_PAGE = "/my-page",
+  SEARCH = "/search",
+}
+
+const cx = {
+  button:
+    // eslint-disable-next-line lingui/no-unlocalized-strings
+    "flex flex-1 justify-center p-5 pb-[max(1.25rem,env(safe-area-inset-bottom))]",
+};
+
 export const DomainBottomNavigation = () => {
   const { path, replace } = useRouter();
   const { myInfo } = useUser();
-  const onClick = (pathname: string) => {
+  const onClick = (pathname: Path) => {
     if (pathname === path) {
       window.scrollTo({ behavior: "smooth", top: 0 });
     } else {
@@ -19,27 +31,27 @@ export const DomainBottomNavigation = () => {
     <BottomNavigation
       menus={[
         <button
-          className={`flex flex-1 justify-center ${path === "/search" ? "text-black" : "text-zinc-400"}`}
-          key="search"
-          onClick={() => onClick("/search")}
+          className={`${cx.button} ${path === Path.SEARCH ? "text-black" : "text-zinc-400"}`}
+          key={Path.SEARCH}
+          onClick={() => onClick(Path.SEARCH)}
         >
           <Search />
         </button>,
         <button
-          className={`flex flex-1 justify-center ${path === "/like" ? "text-black" : "text-zinc-400"}`}
-          key="like"
-          onClick={() => onClick("/like")}
+          className={`${cx.button} ${path === Path.LIKE ? "text-black" : "text-zinc-400"}`}
+          key={Path.LIKE}
+          onClick={() => onClick(Path.LIKE)}
         >
           <Star />
         </button>,
         <button
-          className="flex flex-1 justify-center"
-          key="myPage"
-          onClick={() => onClick("/my-page")}
+          className={cx.button}
+          key={Path.MY_PAGE}
+          onClick={() => onClick(Path.MY_PAGE)}
         >
           <img
             alt=""
-            className={`h-6 w-6 rounded-full border-[1.5px] bg-zinc-200 object-cover ${path === "/my-page" ? "border-black" : "border-transparent"}`}
+            className={`size-6 rounded-full border-[1.5px] bg-zinc-200 object-cover ${path === Path.MY_PAGE ? "border-black" : "border-transparent"}`}
             src={myInfo?.profileImageUrl ?? "/asset/default_user_profile.png"}
           />
         </button>,
