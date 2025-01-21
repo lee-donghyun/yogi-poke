2d554c4fd6c78d9ac4d9b8705b033c556e9ae59c
Donghyun Lee <leedonghyun022@gmail.com>
2025-01-21 16:58:13 +0900

    feat: 네비게이션 ui 개선

--- client/src/ui/base/BottomNavigation.tsx
+++ client/src/ui/base/BottomNavigation.tsx
@@ -2,7 +2,7 @@ import { JSX } from "react";
 
 export const BottomNavigation = ({ menus }: { menus: JSX.Element[] }) => {
   return (
-    <div className="fixed inset-x-0 bottom-0 z-10 bg-white p-5 pb-[max(1.25rem,env(safe-area-inset-bottom))]">
+    <div className="fixed inset-x-0 bottom-0 z-10 bg-white">
       <div className="flex">{menus}</div>
     </div>
   );
