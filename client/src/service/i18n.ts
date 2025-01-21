b47af03a5722aa131c4e5ef6db74eceb8bff68b3
Donghyun Lee <leedonghyun022@gmail.com>
2025-01-21 17:30:55 +0900

    refactor: i18n 서비스 추가

---
+++ client/src/service/i18n.ts
@@ -0,0 +1,38 @@
+import { i18n, Messages } from "@lingui/core";
+import { detect, fromNavigator, fromStorage } from "@lingui/detect-locale";
+
+export const LOCALE_PERSIST_KEY = "LOCALE";
+export enum Locale {
+  EN = "en",
+  JA = "ja",
+  KO = "ko",
+}
+const DEFAULT_LOCALE = Locale.KO;
+
+const isValidLocale = (locale: unknown): locale is Locale =>
+  [Locale.EN, Locale.JA, Locale.KO].includes(locale as Locale);
+
+const detectLocale = () => {
+  const detected = detect(
+    fromStorage(LOCALE_PERSIST_KEY),
+    fromNavigator(),
+  )?.split("-")[0];
+
+  if (isValidLocale(detected)) {
+    return detected;
+  }
+
+  return DEFAULT_LOCALE;
+};
+
+export const setLocale = async (locale: Locale) => {
+  localStorage.setItem(LOCALE_PERSIST_KEY, locale);
+  const { messages } = (await import(
+    `../../locales/${locale}/messages.po`
+  )) as {
+    messages: Messages;
+  };
+  i18n.loadAndActivate({ locale, messages });
+};
+
+export const initLocale = () => setLocale(detectLocale());
