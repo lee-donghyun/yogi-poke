07f816a16c32aaeda556a9dc3d5fa5afbce91409
Donghyun Lee <leedonghyun022@gmail.com>
2025-01-27 19:22:53 +0900

    feat: 팔로잉, 언팔로잉

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -2,6 +2,7 @@ import { forwardRef, Module } from '@nestjs/common';
 import { AuthModule } from 'src/auth/auth.module';
 import { MateModule } from 'src/mate/mate.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { RelationModule } from 'src/relation/relation.module';
 
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
@@ -9,7 +10,12 @@ import { UserService } from './user.service';
 @Module({
   controllers: [UserController],
   exports: [UserService],
-  imports: [PrismaModule, AuthModule, forwardRef(() => MateModule)],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    forwardRef(() => MateModule),
+    forwardRef(() => RelationModule),
+  ],
   providers: [UserService],
 })
 export class UserModule {}
