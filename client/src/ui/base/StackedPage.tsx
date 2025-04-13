31cb67852b55e2035cb1677e64ea88cb9d5c442c
Donghyun Lee <leedonghyun022@gmail.com>
2025-04-13 17:41:21 +0900

    fix: type

--- client/src/ui/base/StackedPage.tsx
+++ client/src/ui/base/StackedPage.tsx
@@ -6,12 +6,19 @@ import { Layer } from "~/ui/provider/StackedLayerProvider";
 const StackedPage = ({
   children,
   close,
+  descriptionId,
+  titleId,
   visible,
 }: {
   children: ReactNode;
 } & Omit<Parameters<Layer>[0], "context">) => {
   return (
-    <Backdrop close={close} visible={visible}>
+    <Backdrop
+      close={close}
+      descriptionId={descriptionId}
+      titleId={titleId}
+      visible={visible}
+    >
       <div
         className={`fixed inset-0 z-40 overflow-y-scroll ${
           visible ? "animate-stacked-layer-from" : "animate-stacked-layer-to"
@@ -28,9 +35,26 @@ export const createStackedPage = (Content: Layer): Layer => {
     console.warn("StackedPage is created. This must be created once.");
   }
 
-  const Component: Layer = ({ close, context, visible }) => (
-    <StackedPage close={close} visible={visible}>
-      <Content close={close} context={context} visible={visible} />
+  const Component: Layer = ({
+    close,
+    context,
+    descriptionId,
+    titleId,
+    visible,
+  }) => (
+    <StackedPage
+      close={close}
+      descriptionId={descriptionId}
+      titleId={titleId}
+      visible={visible}
+    >
+      <Content
+        close={close}
+        context={context}
+        descriptionId={descriptionId}
+        titleId={titleId}
+        visible={visible}
+      />
     </StackedPage>
   );
   return Component;
