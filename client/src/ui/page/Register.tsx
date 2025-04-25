1925599b60fdbb25e8f3acc6e9d5524af1f3db82
Donghyun Lee <leedonghyun022@gmail.com>
2025-04-25 12:19:03 +0900

    feat: 접근성 테스트 추가

--- client/src/ui/page/Register.tsx
+++ client/src/ui/page/Register.tsx
@@ -116,6 +116,7 @@ export const Register = () => {
       <div className="h-40"></div>
       <form
         className="flex flex-col p-5 duration-300"
+        data-testid="회원가입 폼"
         id="register"
         onSubmit={(e) => {
           e.preventDefault();
@@ -124,8 +125,9 @@ export const Register = () => {
         style={{ transform: `translateY(${(step - 3) * 128}px)` }}
       >
         <div
+          aria-hidden={step < 3}
           className={cx.formItem}
-          style={step > 2 ? undefined : { opacity: 0, pointerEvents: "none" }}
+          style={step < 3 ? { opacity: 0, pointerEvents: "none" } : undefined}
         >
           <label className={cx.label} htmlFor="password">
             <Trans>비밀번호</Trans>
@@ -147,8 +149,9 @@ export const Register = () => {
           )}
         </div>
         <div
+          aria-hidden={step < 2}
           className={cx.formItem}
-          style={step > 1 ? undefined : { opacity: 0, pointerEvents: "none" }}
+          style={step < 2 ? { opacity: 0, pointerEvents: "none" } : undefined}
         >
           <label className={cx.label} htmlFor="name">
             <Trans>이름</Trans>
@@ -192,7 +195,6 @@ export const Register = () => {
             <p className={cx.helper}>{translatedCurrentFieldError}</p>
           )}
         </div>
-        <button disabled={isMutating || hasError} />
       </form>
       <div className="fixed inset-x-0 bottom-0 bg-linear-to-b from-transparent to-white p-5 pb-[max(1.25rem,env(safe-area-inset-bottom))]">
         <button
--- client/tests/page.register.spec.ts
+++ client/tests/page.register.spec.ts
@@ -18,6 +18,21 @@ test.beforeEach("set base url", async ({ page }) => {
   });
 });
 
+test.afterEach(async ({ page }) => {
+  const [request] = await Promise.all([
+    page.waitForRequest("**/api/user/register"),
+    page.getByTestId("회원가입 버튼").click(),
+  ]);
+
+  expect(request.method()).toBe("POST");
+  expect(request.postDataJSON()).toEqual({
+    email: "testing",
+    name: "testing",
+    password: "testing",
+    referrerId: null,
+  });
+});
+
 test.describe("회원가입", () => {
   test("마우스로 회원가입", async ({ page }) => {
     await expect(page.getByTestId("이메일")).toBeFocused();
@@ -36,19 +51,6 @@ test.describe("회원가입", () => {
     await expect(page.getByTestId("회원가입 버튼")).toBeDisabled();
     await page.getByTestId("비밀번호").fill("testing");
     await expect(page.getByTestId("회원가입 버튼")).toBeEnabled();
-
-    const [request] = await Promise.all([
-      page.waitForRequest("**/api/user/register"),
-      page.getByTestId("회원가입 버튼").click(),
-    ]);
-
-    expect(request.method()).toBe("POST");
-    expect(request.postDataJSON()).toEqual({
-      email: "testing",
-      name: "testing",
-      password: "testing",
-      referrerId: null,
-    });
   });
   test("키보드로 회원가입", async ({ page }) => {
     await expect(page.getByTestId("이메일")).toBeFocused();
@@ -67,18 +69,60 @@ test.describe("회원가입", () => {
     await expect(page.getByTestId("회원가입 버튼")).toBeDisabled();
     await page.getByTestId("비밀번호").fill("testing");
     await expect(page.getByTestId("회원가입 버튼")).toBeEnabled();
+  });
+  test("유저 입력 검증", async ({ page }) => {
+    await page.getByTestId("이메일").click();
+    await page.getByTestId("이메일").fill("유효하지 않은 id");
+    expect(page.getByTestId("회원가입 버튼")).toBeDisabled();
+    await page.getByTestId("이메일").click();
+    await page.getByTestId("이메일").fill("testing");
+    expect(page.getByTestId("회원가입 버튼")).toBeEnabled();
+    await page.getByTestId("회원가입 버튼").click();
 
-    const [request] = await Promise.all([
-      page.waitForRequest("**/api/user/register"),
-      page.keyboard.press("Enter"),
-    ]);
+    await page.getByTestId("이름").fill("유효한 이름");
+    expect(page.getByTestId("회원가입 버튼")).toBeEnabled();
+    await page.getByTestId("이름").fill("");
+    expect(page.getByTestId("회원가입 버튼")).toBeDisabled();
+    await page.getByTestId("이름").fill("testing");
+    await page.getByTestId("회원가입 버튼").click();
 
-    expect(request.method()).toBe("POST");
-    expect(request.postDataJSON()).toEqual({
-      email: "testing",
-      name: "testing",
-      password: "testing",
-      referrerId: null,
-    });
+    await page.getByTestId("비밀번호").fill("testing");
+    expect(page.getByTestId("회원가입 버튼")).toBeEnabled();
+    await page.getByTestId("비밀번호").fill("");
+    expect(page.getByTestId("회원가입 버튼")).toBeDisabled();
+    await page.getByTestId("비밀번호").fill("testing");
+  });
+
+  test("숨심 요소에 대해 aria-hidden 테스트", async ({ page }) => {
+    await page.goto("http://127.0.0.1:5173/register?is-pwa=1");
+    await expect(page.getByTestId("회원가입 폼")).toMatchAriaSnapshot(`
+    - text: 아이디
+    - textbox "아이디"
+    - paragraph: 소문자 알파벳과 숫자, 언더바(_)만 사용가능합니다.
+    `);
+    await page.getByTestId("이메일").click();
+    await page.getByTestId("이메일").fill("testing");
+    await page.getByTestId("회원가입 버튼").click();
+    await expect(page.getByTestId("회원가입 폼")).toMatchAriaSnapshot(`
+    - text: 이름
+    - textbox "이름"
+    - paragraph: 1자리 이상 입력해주세요.
+    - text: 아이디
+    - textbox "아이디": testing
+    `);
+    await page.getByTestId("이름").click();
+    await page.getByTestId("이름").fill("testing");
+    await page.getByTestId("회원가입 버튼").click();
+    await expect(page.getByTestId("회원가입 폼")).toMatchAriaSnapshot(`
+    - text: 비밀번호
+    - textbox "비밀번호"
+    - paragraph: 6자리 이상 입력해주세요.
+    - text: 이름
+    - textbox "이름": testing
+    - text: 아이디
+    - textbox "아이디": testing
+    `);
+    await page.getByTestId("비밀번호").click();
+    await page.getByTestId("비밀번호").fill("testing");
   });
 });
