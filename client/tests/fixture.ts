8ba393934b9e66180b7517c3f8b531a57c091e3d
Donghyun Lee <leedonghyun022@gmail.com>
2025-04-25 22:32:35 +0900

    feat: app, auth fixture 구현 개선

--- client/tests/fixture.ts
+++ client/tests/fixture.ts
@@ -1,45 +1,50 @@
 import { test as base, Page } from "@playwright/test";
 
-export const test = base.extend<{
-  app: Page;
-  auth: { authorize: (valid?: boolean) => Promise<void> };
-}>({
-  app: async ({ page }, use) => {
-    await page.goto("/");
-    await page.evaluate(() => {
-      localStorage.setItem("IS_PWA", "1");
-    });
-    await page.evaluate(() => {
-      // ky 버그
-      // @see https://github.com/microsoft/playwright/issues/6479#issuecomment-2079000370
-      Request.prototype.clone = function () {
-        return this;
-      };
-    });
-    await page.routeFromHAR("tests/har/localhost.har", {
-      url: "**/api/**",
-      notFound: "fallback",
-    });
+export const test = base
+  .extend<{ app: Page }>({
+    app: async ({ browser }, use) => {
+      const context = await browser.newContext();
+      await context.routeFromHAR("tests/har/localhost.har", {
+        url: "**/api/**",
+        notFound: "fallback",
+      });
 
-    await use(page);
-  },
-  auth: async ({ page }, use) => {
-    await page.route("**/api/user/my-info", async (route) => {
-      const token = route.request().headers()["authorization"];
-      if (token === "VALID") {
-        return route.fulfill();
-      }
-      return route.abort();
-    });
+      const page = await context.newPage();
+      await page.evaluate(() => {
+        // ky 버그
+        // @see https://github.com/microsoft/playwright/issues/6479#issuecomment-2079000370
+        Request.prototype.clone = function () {
+          return this;
+        };
+      });
 
-    const authorize = async (valid: boolean = true) => {
-      await page.evaluate((valid) => {
-        localStorage.setItem("TOKEN", valid ? "VALID" : "INVALID");
-      }, valid);
+      await page.goto("/");
+      await page.evaluate(() => {
+        self.localStorage.setItem("IS_PWA", "1");
+        self.localStorage.setItem("LOCALE", "ko");
+      });
       await page.reload();
-    };
-    await use({ authorize });
-  },
-});
+      await use(page);
+    },
+  })
+  .extend<{ auth: { authorize: (valid?: boolean) => Promise<void> } }>({
+    auth: async ({ app }, use) => {
+      await app.route("**/api/user/my-info", async (route) => {
+        const token = route.request().headers()["authorization"];
+        if (token === "VALID") {
+          return route.fulfill();
+        }
+        return route.fulfill({ status: 403 });
+      });
+
+      const authorize = async (valid: boolean = true) => {
+        await app.evaluate((valid) => {
+          localStorage.setItem("TOKEN", valid ? "VALID" : "INVALID");
+        }, valid);
+        await app.reload();
+      };
+      await use({ authorize });
+    },
+  });
 
 export { expect } from "@playwright/test";
