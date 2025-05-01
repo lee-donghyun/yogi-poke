9fb056afd68cc9189b853e5ea17bcf3af6528aab
Donghyun Lee <leedonghyun022@gmail.com>
2025-05-01 14:26:58 +0900

    feat: dom간 위치 계산 브라우저에 위임

--- client/src/ui/base/Image.tsx
+++ client/src/ui/base/Image.tsx
@@ -1,8 +1,7 @@
 import { ImgHTMLAttributes, useEffect, useRef, useState } from "react";
-import { createPortal } from "react-dom";
+import { createPortal, flushSync } from "react-dom";
 import { RemoveScroll } from "react-remove-scroll";
 
-import { useDebouncedValue } from "~/hook/base/useDebouncedValue";
 import { useKeyboard } from "~/hook/base/useKeyboard";
 
 enum View {
@@ -18,66 +17,20 @@ type ImageProps = {
 > &
   Required<Pick<ImgHTMLAttributes<HTMLImageElement>, "alt" | "src">>;
 
-const THUMBNAIL_TRANSITION_NAME = "image-thumbnail";
-const VIEWER_TRANSITION_NAME = "image-viewer";
-const ANIMATION_DURATION = 150;
+const TRANSITION_NAME = "thumbnail";
 const VIEWER_SIZE_IN_VW = 64;
 
 export const Image = ({ alt, size, src, ...props }: ImageProps) => {
-  const thumbnailRef = useRef(null as unknown as HTMLImageElement);
-  const viewerRef = useRef(null as unknown as HTMLImageElement);
   const backdropRef = useRef<HTMLButtonElement>(null);
   const [view, setView] = useState(View.THUMBNAIL);
-  const debouncedIsViewer = useDebouncedValue(
-    view === View.VIEWER,
-    ANIMATION_DURATION,
-    { strategy: "latest" },
-  );
-
-  const mounted = debouncedIsViewer || view === View.VIEWER;
-  const visible = debouncedIsViewer && view === View.VIEWER;
 
   const openViewer = () => {
     if (!document.startViewTransition) {
       return setView(View.VIEWER);
     }
 
-    thumbnailRef.current.style.viewTransitionName = THUMBNAIL_TRANSITION_NAME;
-
-    const transition = document.startViewTransition(() => setView(View.VIEWER));
-
-    void transition.ready.then(() => {
-      const thumbnail = thumbnailRef.current.getBoundingClientRect();
-      const vw = window.innerWidth / 100;
-      const vh = window.innerHeight / 100;
-
-      const scale = (VIEWER_SIZE_IN_VW * vw) / size;
-      const translateX = -thumbnail.left + 50 * vw - size / 2;
-      const translateY = -thumbnail.top + 50 * vh - size / 2;
-
-      document.documentElement.animate(
-        [
-          {
-            opacity: 1,
-            // eslint-disable-next-line lingui/no-unlocalized-strings
-            transform: `translate(0, 0) scale(1)`,
-          },
-          {
-            opacity: 1,
-            // eslint-disable-next-line lingui/no-unlocalized-strings
-            transform: `translate(${translateX}px, ${translateY}px) scale(${scale})`,
-          },
-        ],
-        {
-          duration: ANIMATION_DURATION,
-          fill: "both",
-          pseudoElement: `::view-transition-old(${THUMBNAIL_TRANSITION_NAME})`,
-        },
-      );
-    });
-
-    void transition.finished.then(() => {
-      thumbnailRef.current.style.viewTransitionName = "";
+    document.startViewTransition(() => {
+      flushSync(() => setView(View.VIEWER));
     });
   };
 
@@ -86,40 +39,8 @@ export const Image = ({ alt, size, src, ...props }: ImageProps) => {
       return setView(View.THUMBNAIL);
     }
 
-    viewerRef.current.style.viewTransitionName = VIEWER_TRANSITION_NAME;
-
-    const transition = document.startViewTransition(() =>
-      setView(View.THUMBNAIL),
-    );
-
-    void transition.ready.then(() => {
-      const thumbnail = thumbnailRef.current.getBoundingClientRect();
-      const vw = window.innerWidth / 100;
-      const vh = window.innerHeight / 100;
-
-      const scale = size / (VIEWER_SIZE_IN_VW * vw);
-      const translateX = -50 * vw + size / 2 + thumbnail.left;
-      const translateY = -50 * vh + size / 2 + thumbnail.top;
-
-      document.documentElement.animate(
-        [
-          {
-            opacity: 1,
-            // eslint-disable-next-line lingui/no-unlocalized-strings
-            transform: `translate(0, 0) scale(1)`,
-          },
-          {
-            opacity: 1,
-            // eslint-disable-next-line lingui/no-unlocalized-strings
-            transform: `translate(${translateX}px, ${translateY}px) scale(${scale})`,
-          },
-        ],
-        {
-          duration: ANIMATION_DURATION,
-          fill: "both",
-          pseudoElement: `::view-transition-old(${VIEWER_TRANSITION_NAME})`,
-        },
-      );
+    document.startViewTransition(() => {
+      flushSync(() => setView(View.THUMBNAIL));
     });
   };
 
@@ -139,26 +60,33 @@ export const Image = ({ alt, size, src, ...props }: ImageProps) => {
   return (
     <>
       <button onClick={openViewer}>
-        <img
-          {...props}
-          alt={alt}
-          className={`rounded-full object-cover ${mounted ? "invisible" : ""}`}
-          ref={thumbnailRef}
-          src={src}
-          style={{ height: size, width: size }}
-        />
+        {view === View.THUMBNAIL && (
+          <img
+            {...props}
+            alt={alt}
+            className="rounded-full object-cover"
+            src={src}
+            style={{
+              height: size,
+              viewTransitionName: TRANSITION_NAME,
+              width: size,
+            }}
+          />
+        )}
+        {view === View.VIEWER && (
+          <div
+            style={{
+              height: size,
+              width: size,
+            }}
+          />
+        )}
       </button>
-      {mounted &&
+      {view === View.VIEWER &&
         createPortal(
           <RemoveScroll>
             <div aria-modal="true" role="dialog">
-              <div
-                className={`fixed inset-0 z-50 backdrop-blur-md backdrop-brightness-95 ${
-                  view === View.VIEWER ? "animate-fade-in" : "animate-fade-out"
-                }`}
-                key={view} // view 가 바뀌었을때 animation이 처음부터 실행되도록
-                style={{ animationDuration: `${ANIMATION_DURATION}ms` }}
-              ></div>
+              <div className="fixed inset-0 z-50 backdrop-blur-md backdrop-brightness-95"></div>
               <button
                 className="fixed inset-0 z-50 flex items-center justify-center"
                 onClick={(e) => {
@@ -171,11 +99,11 @@ export const Image = ({ alt, size, src, ...props }: ImageProps) => {
                 <img
                   {...props}
                   alt={alt}
-                  className={`rounded-full object-cover ${visible ? "" : "invisible"}`}
-                  ref={viewerRef}
+                  className="rounded-full object-cover"
                   src={src}
                   style={{
                     height: `${VIEWER_SIZE_IN_VW}vw`,
+                    viewTransitionName: TRANSITION_NAME,
                     width: `${VIEWER_SIZE_IN_VW}vw`,
                   }}
                 />
